c9a345e246bc9db1cdc362ffd0cdbf742043da45
Ferdous Mannan Shajib <153799536+supahaxxa@users.noreply.github.com>
2025-06-15 10:57:23 +0600

    updated level-builder.py

--- level-builder.py
+++ level-builder.py
@@ -1,7 +1,11 @@
-import assets.warehouse as data
-import os
-import pygame
+from os import getcwd
+from os.path import exists
 from time import sleep
+import library.functions as fx
+import library.warehouse as data
+import pygame
+
+cwd = getcwd()
 
 pygame.init()
 font = pygame.font.SysFont("Arial", 12)
@@ -9,30 +13,21 @@ font = pygame.font.SysFont("Arial", 12)
 WIDTH, HEIGHT = 720, 720
 BLACK = "#000000"
 GRAY = "#7f7f7f"
-UNKNOWN = "#ffeedd"
+BONE_WHITE = "#e7decc"
 
 grid_size = 11
-grid = [-1] * grid_size * grid_size		# filling grid with white color
+grid = [-1] * grid_size * grid_size
 left_margin = (WIDTH - 50 * grid_size) // 2
 top_margin = 50
 x = y = 0
 
+if not exists(data.levels_file_to_open):
+	with open(data.levels_file_to_open, 'w') as file:
+		file.write('')
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Queens - Level Builder")
 
-file_to_write_in = "assets\\linkedin_levels.csv"		# relative path to the grid data storage file
-if not os.path.exists(file_to_write_in):		# checking whether the file exists already
-	with open(file_to_write_in, 'w') as file:		# creating it, if not
-		file.write('')
-
-# function to display text
-def render_text(text, left, top):
-	text = font.render(text, True, BLACK)
-	text_rect = text.get_rect()
-	text_rect.center = (left, top)
-	screen.blit(text, text_rect)
-
-# function to display the grid - tiles, gridlines
 def draw_grid(n):
 	for fk in range(n * n):
 		fi, fj = fk // n, fk % n
@@ -62,22 +57,19 @@ def draw_grid(n):
 
 	del fi, fj, fk, bgcolor, left, top
 
-# function to display the available reference tile colors
 def draw_palette():
 	for fi in range(10):
 		left, top = 40, fi * 50 + 65
 		pygame.draw.circle(screen, data.colors[fi], (left, top), 15)
-		render_text(str(fi), left, top + 25)
+		fx.render_text(screen, str(fi), font, BLACK, left, top + 25)
 
-# function to display the current position of cursor, the tile to be "painted"
 def draw_cursor(li, ti):
 	pygame.draw.circle(screen, GRAY, (li * 50 + left_margin + 25, ti * 50 + top_margin + 25), 3)
 
-# function to store the grid data in the specified CSV file
 def record_data():
 	new_line = ','.join([*map(str, grid)])
 
-	with open(file_to_write_in, 'a') as f_file:
+	with open(data.levels_file_to_open, 'a') as f_file:
 		f_file.write(new_line + '\n')
 
 running = True
@@ -88,32 +80,33 @@ while running:
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_ESCAPE:
 				running = False
-			if event.key in {pygame.K_LEFT, pygame.K_a}:		# for moving cursor
+			if event.key in {pygame.K_LEFT, pygame.K_a}:
 				x = (x - 1) % grid_size
-			if event.key in {pygame.K_RIGHT, pygame.K_d}:		# for moving cursor
+			if event.key in {pygame.K_RIGHT, pygame.K_d}:
 				x = (x + 1) % grid_size
-			if event.key in {pygame.K_UP, pygame.K_w}:		# for moving cursor
+			if event.key in {pygame.K_UP, pygame.K_w}:
 				y = (y - 1) % grid_size
-			if event.key in {pygame.K_DOWN, pygame.K_s}:		# for moving cursor
+			if event.key in {pygame.K_DOWN, pygame.K_s}:
 				y = (y + 1) % grid_size
-			if pygame.K_0 <= event.key <= pygame.K_9:		# for "painting" the tile at current cursor position
+			if pygame.K_0 <= event.key <= pygame.K_9:
 				grid[y * grid_size + x] = event.key - pygame.K_0
-			if event.key == pygame.K_PAGEUP and grid_size < 11:		# for increasing grid_size
+			if event.key == pygame.K_PAGEUP and grid_size < 11:
 				grid_size += 1
 				grid = [-1] * grid_size * grid_size
 				left_margin = (WIDTH - 50 * grid_size) // 2
 				x = y = 0
-			if event.key == pygame.K_PAGEDOWN and grid_size > 5:		# for decreasing grid_size
+			if event.key == pygame.K_PAGEDOWN and grid_size > 5:
 				grid_size -= 1
 				grid = [-1] * grid_size * grid_size
 				left_margin = (WIDTH - 50 * grid_size) // 2
 				x = y = 0
-			if event.key == pygame.K_RETURN and -1 not in grid:		# for storing the grid data
+			if event.key == pygame.K_RETURN and -1 not in grid:
 				record_data()
+				print("[i] Stored a level.")
 				grid = [-1] * grid_size * grid_size
 				x = y = 0
 
-	screen.fill(UNKNOWN)
+	screen.fill(BONE_WHITE)
 
 	draw_grid(grid_size)
 	draw_palette()
@@ -121,4 +114,4 @@ while running:
 
 	pygame.display.update()
 
-	sleep(0.01)		# for adjusting framerate
+	sleep(0.02)
