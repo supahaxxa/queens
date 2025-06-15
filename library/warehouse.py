686a47cfb21b3bd7a3f2bf2677a6501ac5caa323
Ferdous Mannan Shajib <153799536+supahaxxa@users.noreply.github.com>
2025-06-15 10:40:02 +0600

    updated and moved warehouse.py

---
+++ library/warehouse.py
@@ -0,0 +1,9 @@
+# light blue, lavender, light gray, rose, aqua, pink, light orange, light green, lime, tan, white
+colors = [
+	"#96bdfe", "#bba3e1", "#dfdfdf", "#fe7b5f", "#95cace", "#dfa0bf", "#feca91", "#b3dfa0", "#e6f389", "#b9b29f",
+	"#ffffff"
+]
+
+current_screen = 0
+levels_file_to_open = "assets\\levels.csv"
+level_to_open = None
